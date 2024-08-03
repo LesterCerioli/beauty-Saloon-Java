c7ceca68847d06388a1e1fc7a2ea5d173e296903
unknown <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-08-03 01:40:52 -0300

    Initial repository

---
+++ src/main/java/com/lucastechnologyservice/saloonbeauty/SaloonbeautyApplication.java
@@ -0,0 +1,13 @@
+package com.lucastechnologyservice.saloonbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaloonbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaloonbeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucastechnologyservice/saloonbeauty/SaloonbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucastechnologyservice.saloonbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaloonbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
